90a3d62d0f00408c088202be8ff79cc787c7b9b7
minhaj hossain payel <payel922009@gmail.com>
2026-09-25 10:11:56 +0200

    after recurssion and creating linkedlist file

---
+++ linkedlist/linkedlist.js
@@ -0,0 +1,4 @@
+/*linked list - It is linear data structure . Nodes are linked together by a reference field 
+two types of linked list - singly linkedlist and doubly linked list. */
+
+//design a linked list #
